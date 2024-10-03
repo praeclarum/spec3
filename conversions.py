30b716662e850742ce10d895680ef736ed300988
Frank A. Krueger <fak@praeclarum.org>
2024-10-03 09:46:04 -0700

    Add RGB to CIE XYZ conversion

--- conversions.py
+++ conversions.py
@@ -2,6 +2,14 @@
 
 import torch
 
+RGB_to_XYZ_matrix = torch.tensor([
+    [0.4124564, 0.3575761, 0.1804375],
+    [0.2126729, 0.7151522, 0.0721750],
+    [0.0193339, 0.1191920, 0.9503041]
+])
+
+XYZ_to_RGB_matrix = torch.inverse(RGB_to_XYZ_matrix)
+
 def batched_sRGB_to_RGB(srgb):
     """Converts sRGB to linear RGB using a gamma of 2.4.
 
@@ -25,12 +33,6 @@ def batched_RGB_to_sRGB(rgb):
     """
     return torch.where(rgb <= 0.0031308, rgb * 12.92, 1.055 * (rgb ** (1 / 2.4)) - 0.055)
 
-RGB_to_XYZ_matrix = torch.tensor([
-    [0.4124564, 0.3575761, 0.1804375],
-    [0.2126729, 0.7151522, 0.0721750],
-    [0.0193339, 0.1191920, 0.9503041]
-])
-
 def batched_RGB_to_XYZ(rgb):
     """Converts linear RGB to CIE XYZ.
 
@@ -42,21 +44,41 @@ def batched_RGB_to_XYZ(rgb):
     """
     return torch.matmul(rgb, RGB_to_XYZ_matrix)
 
+def batched_XYZ_to_RGB(xyz):
+    """Converts CIE XYZ to linear RGB.
+
+    Args:
+        xyz: A tensor of CIE XYZ values shaped as (batch_size, 3).
+
+    Returns:
+        A tensor of linear RGB values shaped as (batch_size, 3).
+    """
+    return torch.matmul(xyz, XYZ_to_RGB_matrix)
+
 def test_round_trip(title, input_data, conversion_fn, inverse_fn):
     """Tests that the conversion and inverse functions are consistent."""
-    print(f"Testing {title} conversion")
+    print(f"Testing {title} conversion...")
     output_data = conversion_fn(input_data)
     inv_input_data = inverse_fn(output_data)
     n = input_data.shape[0]
     errors = torch.sum((inv_input_data - input_data)**2, dim=1)
     mse_error = torch.mean(errors)
-    for i in range(min(n, 10)):
+    for i in range(min(n, 3)):
         error = errors[i]
-        print(f"error={error:.4f} from {input_data[i]} -> {output_data[i]} -> {inv_input_data[i]}")
-    print(f"Total mean squared error: {mse_error:.4f}")
+        print(f"  error={error:.4f} from {input_data[i]} to {output_data[i]} back to {inv_input_data[i]}")
+    print(f"{title} mean squared error: {mse_error:.4f}")
 
 def test_sRGB_RGB():
     input_data = torch.rand(100, 3)
     test_round_trip("sRGB to RGB", input_data, batched_sRGB_to_RGB, batched_RGB_to_sRGB)
 
-test_sRGB_RGB()
+def test_RGB_XYZ():
+    input_data = torch.rand(100, 3)
+    test_round_trip("RGB to XYZ", input_data, batched_RGB_to_XYZ, batched_XYZ_to_RGB)
+
+def test_all_conversions():
+    test_sRGB_RGB()
+    test_RGB_XYZ()
+
+if __name__ == "__main__":
+    test_all_conversions()
