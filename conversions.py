18355a2e8896d5cd48bbb71e064951df6e62dcee
Frank A. Krueger <fak@praeclarum.org>
2024-10-03 09:40:01 -0700

    Add sRGB to RGB conversion

---
+++ conversions.py
@@ -0,0 +1,62 @@
+"""Collection of functions to convert between different color spaces."""
+
+import torch
+
+def batched_sRGB_to_RGB(srgb):
+    """Converts sRGB to linear RGB using a gamma of 2.4.
+
+    Args:
+        srgb: A tensor of sRGB values in the range [0, 1]
+              shaped as (batch_size, 3).
+
+    Returns:
+        A tensor of linear RGB values shaped as (batch_size, 3).
+    """
+    return torch.where(srgb <= 0.04045, srgb / 12.92, ((srgb + 0.055) / 1.055) ** 2.4)
+
+def batched_RGB_to_sRGB(rgb):
+    """Converts linear RGB to sRGB using a gamma of 2.4.
+
+    Args:
+        rgb: A tensor of linear RGB values shaped as (batch_size, 3).
+
+    Returns:
+        A tensor of sRGB values in the range [0, 1] shaped as (batch_size, 3).
+    """
+    return torch.where(rgb <= 0.0031308, rgb * 12.92, 1.055 * (rgb ** (1 / 2.4)) - 0.055)
+
+RGB_to_XYZ_matrix = torch.tensor([
+    [0.4124564, 0.3575761, 0.1804375],
+    [0.2126729, 0.7151522, 0.0721750],
+    [0.0193339, 0.1191920, 0.9503041]
+])
+
+def batched_RGB_to_XYZ(rgb):
+    """Converts linear RGB to CIE XYZ.
+
+    Args:
+        rgb: A tensor of linear RGB values shaped as (batch_size, 3).
+
+    Returns:
+        A tensor of CIE XYZ values shaped as (batch_size, 3).
+    """
+    return torch.matmul(rgb, RGB_to_XYZ_matrix)
+
+def test_round_trip(title, input_data, conversion_fn, inverse_fn):
+    """Tests that the conversion and inverse functions are consistent."""
+    print(f"Testing {title} conversion")
+    output_data = conversion_fn(input_data)
+    inv_input_data = inverse_fn(output_data)
+    n = input_data.shape[0]
+    errors = torch.sum((inv_input_data - input_data)**2, dim=1)
+    mse_error = torch.mean(errors)
+    for i in range(min(n, 10)):
+        error = errors[i]
+        print(f"error={error:.4f} from {input_data[i]} -> {output_data[i]} -> {inv_input_data[i]}")
+    print(f"Total mean squared error: {mse_error:.4f}")
+
+def test_sRGB_RGB():
+    input_data = torch.rand(100, 3)
+    test_round_trip("sRGB to RGB", input_data, batched_sRGB_to_RGB, batched_RGB_to_sRGB)
+
+test_sRGB_RGB()
