b2e470c245c1997dbc2ef71f578882375cfaf2f9
Frank A. Krueger <fak@praeclarum.org>
2024-10-04 16:19:46 -0700

    Fix matrix order

--- docs.py
+++ docs.py
@@ -26,25 +26,27 @@ def print_matrix(right_matrix, name, in_names, out_names):
                     prefix = " - "
                 else:
                     prefix = " + "
-            out_expr += prefix + f"{abs(value):.12f} * {in_name}"
+            out_expr += prefix + f"{abs(value):.9f} * {in_name}"
         print(f"    {out_name} = {out_expr}")
     print(f"    return {', '.join(out_names)}")
 
 def print_vectorized_matrix(right_matrix, name, in_name):
     """Writes the code to execute out_vector = matrix * in_vector."""
-    in_channels = right_matrix.shape[0]
-    out_channels = right_matrix.shape[1]
+    left_matrix = right_matrix.T
+    out_channels = left_matrix.shape[0]
+    in_channels = left_matrix.shape[1]
     print(f"fn {name}({in_name}: vec{in_channels}<f32>) -> vec{out_channels}<f32> {{")
+    # In WebGPU, matrix constructors are column-major
     print(f"    const {name}: mat{out_channels}x{in_channels}<f32> = mat{out_channels}x{in_channels}(")
-    for j in range(out_channels):
+    for i in range(in_channels):
         print("        ", end="")
-        for i in range(in_channels):
-            value = right_matrix[i, j].item()
+        for j in range(out_channels):
+            value = left_matrix[j, i].item()
             if value < 0.0:
                 prefix = "-"
             else:
                 prefix = ""
-            print(f"{prefix}{abs(value):.12f}, ", end="")
+            print(f"{prefix}{abs(value):.9f}, ", end="")
         print()
     print("    );")
     print(f"    return {name} * {in_name};")
@@ -52,19 +54,25 @@ def print_vectorized_matrix(right_matrix, name, in_name):
 
 def print_matrices():
     print("```python")
+    print_matrix(conversions.RGB_to_SPEC3_right_matrix, "rgb_to_spec3", ["r", "g", "b"], ["sx", "sy", "sz"])
+    print_matrix(conversions.SPEC3_to_RGB_right_matrix, "spec3_to_rgb", ["sx", "sy", "sz"], ["r", "g", "b"])
+    print()
     print_matrix(conversions.XYZ_to_SPEC3_right_matrix, "xyz_to_spec3", ["x", "y", "z"], ["sx", "sy", "sz"])
     print_matrix(conversions.SPEC3_to_XYZ_right_matrix, "spec3_to_xyz", ["sx", "sy", "sz"], ["x", "y", "z"])
     print()
-    print_matrix(conversions.RGB_to_SPEC3_right_matrix, "rgb_to_spec3", ["r", "g", "b"], ["sx", "sy", "sz"])
-    print_matrix(conversions.SPEC3_to_RGB_right_matrix, "spec3_to_rgb", ["sx", "sy", "sz"], ["r", "g", "b"])
+    print_matrix(conversions.RGB_to_XYZ_right_matrix, "rgb_to_xyz", ["r", "g", "b"], ["x", "y", "z"])
+    print_matrix(conversions.XYZ_to_RGB_right_matrix, "xyz_to_rgb", ["x", "y", "z"], ["r", "g", "b"])
     print("```")
     print()
     print("```rust")
+    print_vectorized_matrix(conversions.RGB_to_SPEC3_right_matrix, "rgb_to_spec3", "rgb")
+    print_vectorized_matrix(conversions.SPEC3_to_RGB_right_matrix, "spec3_to_rgb", "spec3")
+    print()
     print_vectorized_matrix(conversions.XYZ_to_SPEC3_right_matrix, "xyz_to_spec3", "xyz")
     print_vectorized_matrix(conversions.SPEC3_to_XYZ_right_matrix, "spec3_to_xyz", "spec3")
     print()
-    print_vectorized_matrix(conversions.RGB_to_SPEC3_right_matrix, "rgb_to_spec3", "rgb")
-    print_vectorized_matrix(conversions.SPEC3_to_RGB_right_matrix, "spec3_to_rgb", "spec3")
+    print_vectorized_matrix(conversions.RGB_to_XYZ_right_matrix, "rgb_to_xyz", "xyz")
+    print_vectorized_matrix(conversions.XYZ_to_RGB_right_matrix, "xyz_to_rgb", "xyz")
     print("```")
 
 def write_color_table():
